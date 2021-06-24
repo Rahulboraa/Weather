ca5980549975fe090faa5561b6db6fb1d3cd475b
root1 <shubhomj@geniusplaza.com>
2021-06-24 18:11:57 +0530

    search and temprature display

--- Scripts/forcast.js
+++ Scripts/forcast.js
@@ -2,24 +2,36 @@ const key = "80a0479440bebd35c775b20a9f1e8443";
 const baseurl = "https://api.openweathermap.org/data/2.5/weather";
 
 // *Event Listener function on Keypress
-const searchInput = document.querySelector(".form-control");
-searchInput.addEventListener("keypress", (event) => {
-  if (event.keyCode == 13) {
-    console.log(searchInput.value);
-    GetWeather(searchInput.value);
+const inputBox = document.querySelector(".form-control");
+inputBox.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    console.log(inputBox.value);
+    getWeather(inputBox.value);
   }
 });
 
 // *Get Weather Report
-const GetWeather = (city) => {
-  fetch(`${baseurl}?q=${city}$appid=key`)
+function getWeather(city) {
+  fetch(`${baseurl}?q=${city}&appid=${key}&units=metric`)
     .then((weather) => {
       return weather.json();
     })
     .then(showWeather);
-};
+}
 
-// *Show Weather Report
+// *Show Weather Reports
 function showWeather(weather) {
   console.log(weather);
+
+  let city = document.querySelector(".city");
+  city.innerText = `${weather.name},${weather.sys.country}`;
+
+  let weatherCondition = document.querySelector(".weather");
+  weatherCondition.innerText = `${weather.weather[0].main}`;
+
+  let temp = document.querySelector(".temp");
+  temp.innerText = `${Math.round(weather.main.temp)}`;
+
+  // let images = document.querySelector(".images");
+  // images.innerHTML = 
 }
